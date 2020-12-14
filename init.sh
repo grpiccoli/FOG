d3fc62469f1cf80b531deae66d1abc246ec026d7
kiwiepic <grpiccoli@gmail.com>
2020-12-15 12:47:30 +1300

    corrected init link

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #install enhanced mode
 sudo apt install git vim xclip
-wget https://github.com/microsoft/linux-vm-tools/blob/cb07b3eaeb89822ebc6eaddb10f3932bb1879f47/ubuntu/20.04/install.sh
+wget https://raw.githubusercontent.com/microsoft/linux-vm-tools/cb07b3eaeb89822ebc6eaddb10f3932bb1879f47/ubuntu/20.04/install.sh
 chmod +x install.sh
 sudo ./install.sh
 sudo apt autoremove 
