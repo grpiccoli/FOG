ed9d6c6ec8074198ce953344316eddecc6a4eda3
Guillermo Rodriguez Piccoli <guille.arp@gmail.com>
2022-06-08 18:22:04 +1200

    Create 3.RepeatMasker.sh

---
+++ tetools/3.RepeatMasker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=Rmasker
+### each process only uses between 4 to 6 processes even though it says 32
+#SBATCH -c 32
+#SBATCH -o ./%x-%A-%a.out
+#SBATCH -e ./%x-%A-%a.err
+### 
+#SBATCH --time=3-0:00
+#SBATCH --mem=50G
+
+LB=/vuw/tetools/Libraries
+APP=tetools
+s="singularity run -B $scra:/vuw -W /vuw/$APP --env LC_ALL=C --env LIBDIR=$LB --env BLASTDB=/vuw/$APP/databases $scra/$APP/tetools_grpiccoli.sif"
+
+dbs=($(ls $scra/$APP/databases/*.translation | xargs -I{} basename {} .translation))
+
+sOUT=/vuw/$APP/out
+OUT=$scra/$APP/out
+num=${1}
+db=${dbs[$num]}
+mkdir -p $OUT/${db}/Rmasker
+cd $OUT/${db}/Rmasker
+ln -s $OUT/${db}/consensi.fa.classified $OUT/${db}/Rmasker/consensi.fa.classified
+$s RepeatMasker -lib consensi.fa.classified -gff -pa 32 ${db}*.fa
